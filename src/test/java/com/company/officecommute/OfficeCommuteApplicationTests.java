ef3c09947cbfd816cbc49f46a6178ddd2dfaa599
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-05-19 10:35:55 +0900

    chore: backend Springboot initial settings
    * Springboot 3.2.5
    * Java 17
    * gradle 8.5

---
+++ src/test/java/com/company/officecommute/OfficeCommuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.officecommute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeCommuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
