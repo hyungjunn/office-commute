5e6a2ff7530d810eaf30c5195d7cdc591568101c
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-05-19 12:48:00 +0900

    feat: add Employee domain

---
+++ src/main/java/com/company/officecommute/domain/employee/Employee.java
@@ -0,0 +1,11 @@
+package com.company.officecommute.domain.employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+    private String teamName;
+    private Role role;
+    private LocalDate birthday;
+    private LocalDate workStartDate;
+}
