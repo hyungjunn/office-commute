eadbcede7020d96e0bca15ef843c9d9ef59bd31e
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-05-21 14:04:01 +0900

    add Employee entity

--- src/main/java/com/company/officecommute/domain/employee/Employee.java
+++ src/main/java/com/company/officecommute/domain/employee/Employee.java
@@ -1,11 +1,24 @@
 package com.company.officecommute.domain.employee;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 import java.time.LocalDate;
 
+@Entity
 public class Employee {
+
+    @Id @GeneratedValue
+    private Long id;
+
     private String name;
+
     private String teamName;
+
     private Role role;
+
     private LocalDate birthday;
+    
     private LocalDate workStartDate;
 }
