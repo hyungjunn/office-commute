737790e228f862031bc37f000010a8e77b75d0a0
limhjun <limhjun.me@gmail.com>
2026-07-03 11:22:46 +0900

    Remove because of no usage

--- src/test/java/com/company/officecommute/web/TestApi.java
+++
@@ -1,8 +0,0 @@
-package com.company.officecommute.web;
-
-public class TestApi implements ApiProperties {
-    @Override
-    public String combineURL(String solYear, String solMonth) {
-        return "http://test-url.com";
-    }
-}
